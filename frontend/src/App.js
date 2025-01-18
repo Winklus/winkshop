9dfd3506b31efe71376f330e8c7a905fbdd84378
Wink_iho <pdfaxon@gmail.com>
2025-01-18 08:54:30 +0100

    fetching data with API

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,19 @@
+import { Container } from "react-bootstrap";
 import React from "react";
+import { Outlet } from "react-router-dom";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
     <>
-      <h1>hello world</h1>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <Outlet />
+        </Container>
+      </main>
+      <Footer />
     </>
   );
 };
