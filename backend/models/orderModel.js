faf0a4d1115134866aee301cfb21ed78f7fbb4c7
Wink_iho <pdfaxon@gmail.com>
2025-01-23 23:45:03 +0100

    modeling of data

--- backend/models/orderModel.js
+++ backend/models/orderModel.js
@@ -7,7 +7,86 @@ const orderSchema = mongoose.Schema({
     ref: "User",
   },
 
-  orderItems: {
-    
+  orderItems: [
+    {
+    name: {
+      type: String,
+      require: true,
+    },
+    qty: {
+      type: Number,
+      require: true,
+    },
+    image: {
+      type: String,
+      require: true,
+    },
+    price: {
+      type: Number,
+      require: true,
+    },
+    product: {
+      type: mongoose.Schema.Types.ObjectId,
+      require: true,
+      ref: "Product",
+    },
   }
+],
+  shippingAddress: {
+    address: { type: String, require: true },
+    city: { type: String, require: true },
+    postalCode: { type: String, require: true },
+    country: { type: String, require: true },
+  },
+  paymentMethod: {
+    type: String,
+    require: true,
+  },
+  paymentResult: {
+    id: {type: String},
+    status: {type: String},
+    update_time: {type: String},
+    email_address: {type: String},
+  },
+  itemsPrice: {
+    type: Number,
+    require: true,
+    default: 0.0,
+  },
+  taxPrice: {
+    type: Number,
+    required: true,
+    default: 0.0,
+  },
+  shippingPrice: {
+    type: Number,
+    required: true,
+    default: 0.0,
+  },
+  totalPrice: {
+    type: Number,
+    required: true,
+    default: 0.0,
+  },
+  isPaid: {
+    type: Boolean,
+    require: true,
+    default: false,
+  },
+  paidAt: {
+    type: Date,
+  }, 
+  isDelivered: {
+    type: Boolean,
+    require: true,
+    default: false,
+  }, 
+  deliveredAt: {
+    type: Date,
+  },
+},{
+  timestamp: true,
 });
+
+const Order = mongoose.model("Order", orderSchema);
+export default Order;
