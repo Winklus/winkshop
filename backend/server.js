b8fa47ec6825a22411d71e2581c94db1795be481
Wink_iho <pdfaxon@gmail.com>
2025-01-25 01:22:11 +0100

    Get products from database

--- backend/server.js
+++ backend/server.js
@@ -2,24 +2,20 @@ import express from "express";
 import dotenv from "dotenv";
 dotenv.config()
 import connectDB from "./config/db.js";
-import products from "./data/products.js";
+import productRoute from "./routers/productRoute.js";
 const port = process.env.PORT || 5000;
 
 connectDB(); // connect to MongoDB
 
 
 const app = express();
+
 app.get("/", (req, res) => {
   res.send("API is running");
 });
 
-app.get("/api/products", (req, res) => {
-  res.json(products);
-});
+app.use("/api/products", productRoute);
 
-app.get("/api/products/:id", (req, res) => {
-  const product = products.find((p) => p._id === req.params.id);
-  res.json(product);
-});
+
 
 app.listen(port, () => console.log(`server listening on port ${port}`));
