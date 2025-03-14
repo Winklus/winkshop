541613db0637a107fc7c41662362d6f133d11274
Wink_iho <pdfaxon@gmail.com>
2025-03-14 03:04:05 +0100

    preparing for production

--- backend/server.js
+++ backend/server.js
@@ -31,12 +31,6 @@ app.use(cookieParser());
 
 
 
-app.get("/", (req, res) => {
-  res.send("API is running");
-});
-
-
-
 app.use("/api/products", productRoute);
 app.use("/api/users", userRoute);
 app.use("/api/orders", orderRoute);
@@ -44,6 +38,22 @@ app.use("/api/upload", uploadRoute);
 
 const __dirname = path.resolve();
 app.use('/upload', express.static(path.join(__dirname, 'uploads')));
+
+
+if(process.env.NODE_ENV === "production"){
+  // set static folder
+  app.use(express.static(path.join(__dirname, '/frontend/build')));
+
+  // any route that is not api will be redirected to index.html
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html')));
+}else {
+  app.get("/", (req, res) => {
+    res.send("API is running");
+  });
+}
+
 
 app.get("/api/config/paypal", (req, res) => res.send({clientId: process.env.PAYPAL_CLIENT_ID}));
  
